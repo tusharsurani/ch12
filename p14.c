2c8948d2380d9352e194970c413d06542b17017e
tusharsurani <137286868+tusharsurani@users.noreply.github.com>
2023-07-10 09:46:03 +0530

    Add files via upload

---
+++ p14.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+	char i,j,k;
+	for(i='A';i<='E';i++)
+	{
+		for(k=i;k<='E';k++)
+		{
+			printf(" ");
+		}
+		for(j='A';j<=i;j++)
+		{
+			printf("%c",j);
+		}
+		printf("\n");
+	}
+}
