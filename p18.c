ee2248bb3156b95979f2d8b456d235f416db2e3e
tusharsurani <137286868+tusharsurani@users.noreply.github.com>
2023-07-10 09:47:13 +0530

    Add files via upload

---
+++ p18.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+main()
+{
+	int i,j,k;
+	for(i=1;i<=5;i++)
+	{
+		for(k=i;k<=5;k++)
+		{
+			printf(" ");
+		}
+		for(j=i;j>=1;j--)
+		{
+			if(i%2==0)
+			{
+				printf("0");
+			}
+			else
+			{
+				printf("1");
+			}
+		}
+		printf("\n");
+	}
+}
